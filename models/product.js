1e11199647cc218e2aba601a45dd075ffbeab821
prataprohit526 <prataprohit526@gmail.com>
2023-05-02 09:59:42 +0530

    add all

--- models/product.js
+++ models/product.js
@@ -3,9 +3,19 @@ var mongoose = require("mongoose");
 //Complete Schema so that while ading new product it should be valid.
 // No two products can have same name in Document .
 var productSchema = mongoose.Schema({
-  
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
 });
-
-
-
-module.exports = mongoose.model("User", productSchema);;
+module.exports = mongoose.model("User", productSchema);
